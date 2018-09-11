2575cc0d392cdbacccd3a720f5a7aa3ea40669cd
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 18:07:40 -0700

    Further Study

--- ratings.py
+++ ratings.py
@@ -4,6 +4,7 @@
 # put your code here
 
 import sys
+from random import choice
 
 scores_file = sys.argv[1]
 
@@ -23,15 +24,49 @@ for line in open(scores_file):
     score_dict[each_line[0]] = int(each_line[1])
 
 
-restaurant_name = input("Please type a restaurant.").title()
-restaurant_score = int(input("Please type a score."))
-print ("****************************************")
-print()
-score_dict[restaurant_name] = restaurant_score
+while True:
+    # This code is if the user wants to see restaurants and ratings.
+    choice = input("Please enter: 's' to see all ratings, 'a' to add a restaurant/rating, 'u' to update, or 'q' to quit :-")
+    
+    if choice == "s":
+        
+        sorted_tuple = sorted(score_dict.items())
+
+        for line in sorted_tuple:
+            print(f"{line[0]} is rated at {line[1]}.")
+        print()
+
+    elif choice == "a":
+# This code is if the user wants to add a new restaurant and rating.
+        restaurant_name = input("Please type a restaurant.").title()
+        restaurant_score = int(input("Please type a score."))
+
+        while restaurant_score > 5 or restaurant_score < 1:
+            restaurant_score = int(input("Please enter a score between 1 and 5."))
+
+        print ("****************************************")
+        print()
+
+        score_dict[restaurant_name] = restaurant_score
+
+        sorted_tuple = sorted(score_dict.items())
+
+        for line in sorted_tuple:
+            print(f"{line[0]} is rated at {line[1]}.")
+        print()
+
+    elif choice == "u":
+        random_restaurant = choice(sorted_tuple)
+        print(random_restaurant)
 
 
-sorted_tuple = sorted(score_dict.items())
+    elif choice == "q":
+        sys.exit()
 
-for line in sorted_tuple:
-    print(f"{line[0]} is rated at {line[1]}.")
+    else :
+        print("invalid input")
+        continue
+
+
+
 
